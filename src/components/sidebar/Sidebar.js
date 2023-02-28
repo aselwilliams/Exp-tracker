f3a60c38bbdc64f171e1b74cefa1c824256d2318
Asel Williams <aselwilliams22@gmail.com>
2023-02-28 09:39:36 -0600

    uses calc() to calculate dashboard container width

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -10,7 +10,7 @@ import AuthContext from '../../store/authContext';
 
 const Sidebar = () => {
     const { logout} = useContext(AuthContext)
-    // console.log(firstName, 'firstName')
+  
     let username=localStorage.getItem('username')
     let firstName=localStorage.getItem('firstName')
     let lastName=localStorage.getItem('lastName')
